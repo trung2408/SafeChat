039fd912df6bd79757b0cf8ccd854e82418f7467
TrungLH <trungluuhoang456@gmail.com>
2026-03-12 14:53:01 +0700

    Add composite entity ConversationParticipant

--- src/main/java/com/trung/chat/chatapp/entity/ConversationParticipant.java
+++ src/main/java/com/trung/chat/chatapp/entity/ConversationParticipant.java
@@ -1,4 +1,53 @@
 package com.trung.chat.chatapp.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "conversation_participants")
 public class ConversationParticipant {
+    @EmbeddedId
+    private ConversationParticipantId id;
+
+    @ManyToOne
+    @MapsId("userId")
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @MapsId("conversationId")
+    @JoinColumn(name = "conversation_id", nullable = false)
+    private Conversation conversation;
+
+    public ConversationParticipant() {
+    }
+
+    public ConversationParticipant(ConversationParticipantId id, User user, Conversation conversation) {
+        this.id = id;
+        this.user = user;
+        this.conversation = conversation;
+    }
+
+    public ConversationParticipantId getId() {
+        return id;
+    }
+
+    public void setId(ConversationParticipantId id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Conversation getConversation() {
+        return conversation;
+    }
+
+    public void setConversation(Conversation conversation) {
+        this.conversation = conversation;
+    }
 }
