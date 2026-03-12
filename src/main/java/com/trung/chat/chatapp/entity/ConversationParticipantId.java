1939ad709a728d93286cd8a701a690bc4f94b4fe
TrungLH <trungluuhoang456@gmail.com>
2026-03-12 14:51:54 +0700

    Add ConversationParticipantId for embeded Id of Conversation and Participant

--- src/main/java/com/trung/chat/chatapp/entity/ConversationParticipantId.java
+++ src/main/java/com/trung/chat/chatapp/entity/ConversationParticipantId.java
@@ -1,4 +1,33 @@
 package com.trung.chat.chatapp.entity;
 
-public class ConversationParticipantId {
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+@Embeddable
+@Table(name = "conversation_participants")
+public class ConversationParticipantId implements Serializable {
+    @Column(name = "user_id")
+    private UUID userId;
+
+    @Column(name = "conversation_id")
+    private UUID conversationId;
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, conversationId);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null || getClass()!=(obj.getClass())){
+            return false;
+        }
+        ConversationParticipantId that = (ConversationParticipantId) obj;
+        return Objects.equals(userId, that.userId) && Objects.equals(conversationId, that.conversationId);
+    }
 }
