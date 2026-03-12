b8246e4a90198e2fda443637c661202b31afe9ca
TrungLH <trungluuhoang456@gmail.com>
2026-03-12 14:49:43 +0700

    Add entity Message

--- src/main/java/com/trung/chat/chatapp/entity/Message.java
+++ src/main/java/com/trung/chat/chatapp/entity/Message.java
@@ -1,4 +1,68 @@
 package com.trung.chat.chatapp.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "messages")
 public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long messageId;
+    @ManyToOne
+    @JoinColumn(name = "conversation_id", nullable = false)
+    private Conversation conversation;
+    @ManyToOne
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User user;
+    private String content;
+    private LocalDateTime createdAt;
+
+    public Message() {
+    }
+
+    public Message(String content) {
+        this.content = content;
+    }
+
+    public Long getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(Long messageId) {
+        this.messageId = messageId;
+    }
+
+    public Conversation getConversation() {
+        return conversation;
+    }
+
+    public void setConversation(Conversation conversation) {
+        this.conversation = conversation;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
