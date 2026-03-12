f7dee6458a1a0b030533240487eaec9d4d4191c7
TrungLH <trungluuhoang456@gmail.com>
2026-03-12 14:54:47 +0700

    Add MessageRepository

--- src/main/java/com/trung/chat/chatapp/repository/MessageRepository.java
+++ src/main/java/com/trung/chat/chatapp/repository/MessageRepository.java
@@ -1,4 +1,9 @@
 package com.trung.chat.chatapp.repository;
 
-public interface MessageRepository {
+import com.trung.chat.chatapp.entity.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface MessageRepository extends JpaRepository<Message, UUID> {
 }
