ae64a76f3f451370e0d27a282bd6889274812ecc
TrungLH <trungluuhoang456@gmail.com>
2026-03-30 16:47:12 +0700

    Add UserRepository

--- src/main/java/com/trung/chat/safechat/repository/UserRepository.java
+++ src/main/java/com/trung/chat/safechat/repository/UserRepository.java
@@ -1,6 +1,6 @@
-package com.trung.chat.chatapp.repository;
+package com.trung.chat.safechat.repository;
 
-import com.trung.chat.chatapp.entity.User;
+import com.trung.chat.safechat.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
