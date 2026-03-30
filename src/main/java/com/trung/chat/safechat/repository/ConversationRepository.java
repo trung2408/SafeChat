5d9c1d4683b8b78ab347c50443af9b7fb2da56e8
TrungLH <trungluuhoang456@gmail.com>
2026-03-30 16:31:59 +0700

    Add ConversationRepository

--- src/main/java/com/trung/chat/safechat/repository/ConversationRepository.java
+++ src/main/java/com/trung/chat/safechat/repository/ConversationRepository.java
@@ -1,6 +1,6 @@
-package com.trung.chat.chatapp.repository;
+package com.trung.chat.safechat.repository;
 
-import com.trung.chat.chatapp.entity.Conversation;
+import com.trung.chat.safechat.entity.Conversation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
