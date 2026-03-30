0ea897f2f72ef5b57088c4e689fa341a9dd9b60b
TrungLH <trungluuhoang456@gmail.com>
2026-03-30 16:49:14 +0700

    Update Application

--- src/main/java/com/trung/chat/safechat/ChatAppApplication.java
+++ src/main/java/com/trung/chat/safechat/ChatAppApplication.java
@@ -1,4 +1,4 @@
-package com.trung.chat.chatapp;
+package com.trung.chat.safechat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/trung/chat/safechat/ChatAppApplicationTests.java
+++ src/test/java/com/trung/chat/safechat/ChatAppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.trung.chat.chatapp;
+package com.trung.chat.safechat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
