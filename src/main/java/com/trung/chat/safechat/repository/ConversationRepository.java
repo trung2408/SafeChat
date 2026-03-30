dc4d831573e16c0fa6c19ce902b9c7bb91d93557
TrungLH <trungluuhoang456@gmail.com>
2026-03-30 22:49:34 +0700

    Fix ConversationRepository: repare method findPrivateConversation

--- src/main/java/com/trung/chat/safechat/repository/ConversationRepository.java
+++ src/main/java/com/trung/chat/safechat/repository/ConversationRepository.java
@@ -1,11 +1,23 @@
 package com.trung.chat.safechat.repository;
 
 import com.trung.chat.safechat.entity.Conversation;
+import com.trung.chat.safechat.entity.ConversationReadId;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
 import java.util.UUID;
 
 public interface ConversationRepository extends JpaRepository<Conversation, UUID> {
-
+    @Query("""
+        SELECT c FROM Conversation c 
+        WHERE c.type = 'PRIVATE' AND 
+                EXISTS (
+                        SELECT 1 FROM ConversationParticipant p
+                        WHERE p.conversation = c AND p.user = :userA
+                        )
+                AND EXISTS (
+                        SELECT 1 FROM ConversationParticipant p 
+                        WHERE p.conversation = c AND p.user = :userB
+                        )        
+        """)
     Conversation findPrivateConversation(UUID userA, UUID userB);
 }
