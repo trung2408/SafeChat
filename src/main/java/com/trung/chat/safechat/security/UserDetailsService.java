b6caead5c6061bf016ca09ee5d66749e0f466dea
TrungLH <trungluuhoang456@gmail.com>
2026-04-03 05:30:21 +0700

    Add UserDetailsService

--- src/main/java/com/trung/chat/safechat/security/UserDetailsService.java
+++ src/main/java/com/trung/chat/safechat/security/UserDetailsService.java
@@ -1,4 +1,23 @@
 package com.trung.chat.safechat.security;
 
+import com.trung.chat.safechat.entity.User;
+import com.trung.chat.safechat.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+@Service
 public class UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public UserDetails loadUserByUserName(String userId){
+        User user = userRepository.findById(UUID.fromString(userId))
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new CustomUserDetails(user);
+    }
 }
