eb259769f8154855ab1b88ee32330184d14d88e3
TrungLH <trungluuhoang456@gmail.com>
2026-04-03 05:27:51 +0700

    Add CustomUserDetails for getting current user service

--- src/main/java/com/trung/chat/safechat/security/CustomUserDetails.java
+++ src/main/java/com/trung/chat/safechat/security/CustomUserDetails.java
@@ -1,4 +1,39 @@
 package com.trung.chat.safechat.security;
 
-public class CustomUserDetails {
+import com.trung.chat.safechat.entity.User;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+public class CustomUserDetails implements UserDetails {
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_"+user.getRole()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserName();
+    }
+
+
+    public UUID getUserId() {
+        return user.getUserId();
+    }
 }
