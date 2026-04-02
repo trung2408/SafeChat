c12d2327706eaec369a61d866bb215cf43488ff4
TrungLH <trungluuhoang456@gmail.com>
2026-04-03 05:27:08 +0700

    Add RegisterRequest dto

--- src/main/java/com/trung/chat/safechat/dto/RegisterRequest.java
+++ src/main/java/com/trung/chat/safechat/dto/RegisterRequest.java
@@ -1,4 +1,40 @@
 package com.trung.chat.safechat.dto;
 
 public class RegisterRequest {
+    private String email;
+    private String password;
+    private String username;
+
+    public RegisterRequest() {
+    }
+
+    public RegisterRequest(String email, String password, String username) {
+        this.email = email;
+        this.password = password;
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
