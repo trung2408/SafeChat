d7292230166c947ddf75d189420d28daabad5cdd
TrungLH <trungluuhoang456@gmail.com>
2026-04-02 19:05:20 +0700

    Add JwtService for generate and check token service

--- src/main/java/com/trung/chat/safechat/service/JwtService.java
+++ src/main/java/com/trung/chat/safechat/service/JwtService.java
@@ -1,4 +1,40 @@
 package com.trung.chat.safechat.service;
 
+import com.trung.chat.safechat.entity.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+import java.util.UUID;
+
 public class JwtService {
+    private final String SECRET_KEY = "My-Secret-Key-Dcmmmmm";
+
+    public String generateToken(User user){
+        return Jwts.builder()
+                .setSubject(user.getUserId().toString())
+                .claim("role", user.getRole())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public UUID extractUserId(String token){
+        String userId = Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+        return UUID.fromString(userId);
+    }
+
+    public boolean isValidToken(String token){
+        try{
+            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
 }
