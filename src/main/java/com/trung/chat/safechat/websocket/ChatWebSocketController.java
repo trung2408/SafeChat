a0c644c92a4d12aa974ecef38c04248ac8bcd0dd
TrungLH <trungluuhoang456@gmail.com>
2026-04-06 23:35:23 +0700

    Add ChatWebSocketControler

--- src/main/java/com/trung/chat/safechat/websocket/ChatWebSocketController.java
+++ src/main/java/com/trung/chat/safechat/websocket/ChatWebSocketController.java
@@ -1,4 +1,21 @@
 package com.trung.chat.safechat.websocket;
 
+import com.trung.chat.safechat.websocket.dto.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ChatWebSocketController {
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    public ChatWebSocketController(SimpMessagingTemplate simpMessagingTemplate){
+        this.simpMessagingTemplate = simpMessagingTemplate;
+    }
+
+    @MessageMapping("/chat.sendMessage")
+    public void sendMessage(ChatMessage message){
+        System.out.println("Recieved:" + message.getContent());
+        simpMessagingTemplate.convertAndSend("/topic/conversations/" + message.getConversationId(), message);
+    }
 }
