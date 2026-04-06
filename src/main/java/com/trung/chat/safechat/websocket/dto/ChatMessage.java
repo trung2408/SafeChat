b912559a635d7054208efac49c8fca713973cdee
TrungLH <trungluuhoang456@gmail.com>
2026-04-06 23:36:44 +0700

    Add ChatMessage dto for webSocket

--- src/main/java/com/trung/chat/safechat/websocket/dto/ChatMessage.java
+++ src/main/java/com/trung/chat/safechat/websocket/dto/ChatMessage.java
@@ -1,4 +1,30 @@
 package com.trung.chat.safechat.websocket.dto;
 
 public class ChatMessage {
+    private String content;
+    private String conversationId;
+
+    public ChatMessage() {
+    }
+
+    public ChatMessage(String content, String conversationId) {
+        this.content = content;
+        this.conversationId = conversationId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getConversationId() {
+        return conversationId;
+    }
+
+    public void setConversationId(String conversationId) {
+        this.conversationId = conversationId;
+    }
 }
