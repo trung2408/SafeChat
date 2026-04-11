0299a907f65d3d61274e130c4a7584a8d63be48c
TrungLH <trungluuhoang456@gmail.com>
2026-04-11 15:00:02 +0700

    Modifile ConversationParticipantRepository: change method findByUserId to findUserConversations

--- src/main/java/com/trung/chat/safechat/repository/ConversationParticipantRepository.java
+++ src/main/java/com/trung/chat/safechat/repository/ConversationParticipantRepository.java
@@ -4,6 +4,7 @@ import com.trung.chat.safechat.entity.Conversation;
 import com.trung.chat.safechat.entity.ConversationParticipant;
 import com.trung.chat.safechat.entity.ConversationParticipantId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
@@ -11,7 +12,13 @@ import java.util.UUID;
 public interface ConversationParticipantRepository extends JpaRepository<ConversationParticipant, ConversationParticipantId> {
     int countByUser(ConversationParticipant conversationParticipant);
 
-    List<Conversation> findByUserId(UUID userId);
+    @Query("""
+        SELECT cp.conversation FROM ConversationParticipant cp
+        LEFT JOIN cp.conversation.lastMessage lm 
+        WHERE cp.user.id = :userId
+        ORDER BY lm.createdAt DESC 
+""")
+    List<Conversation> findUserConversations(UUID userId);
 
     boolean existsByUserIdAndConversationId(UUID senderId, UUID conversationId);
 }
