2fc7240762a827b53a733a594bff91813b40397a
TrungLH <trungluuhoang456@gmail.com>
2026-04-11 14:57:53 +0700

    Update Conversation: add field lastMessage

--- src/main/java/com/trung/chat/safechat/entity/Conversation.java
+++ src/main/java/com/trung/chat/safechat/entity/Conversation.java
@@ -17,6 +17,12 @@ public class Conversation {
 //    @OneToMany(mappedBy = "conversation", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 //    List<Message> messageList = new ArrayList<>();
     // Trong thực tế, một conversation sẽ có rất nhiều message, nên nếu thiết kế list message nằm toàn bộ trong conversation thì khi query và cần lấy message trong conversation sẽ phải load toàn bộ message, gây tốn tài nguyên và mất rất nhiều thời gian, cách thiết kế đúng là chỉ để message map ManyToOne đến conversation, khi nào cần lấy message thì query trực tiếp bên Message
+
+
+    // Ở đây thêm field lastMessage với quan hệ Many to one ko phải là vì theo logic thực tế nhiều Conversation có 1 lastMessage mà thực tế một Conversation chỉ có một lastMessage. Nhưng để biểu diễn với JPA rằng lastMessage là FK khóa ngoại của Conversation, trỏ đến/tham chiếu đến Message thì phải dùng Many to one vì Many to one luôn biểu diễn khóa ngoại
+    @ManyToOne
+    @JoinColumn(name = "last_message_id")
+    private Message lastMessage;
     private LocalDateTime createdAt;
 
     @PrePersist
@@ -49,6 +55,14 @@ public class Conversation {
 
     public void setType(ConversationType type) {
         this.type = type;
+    }
+
+    public Message getLastMessage() {
+        return lastMessage;
+    }
+
+    public void setLastMessage(Message lastMessage) {
+        this.lastMessage = lastMessage;
     }
 
     public LocalDateTime getCreatedAt() {
