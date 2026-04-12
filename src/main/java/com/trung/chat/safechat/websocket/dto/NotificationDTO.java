33a2b64fcc91256dc732ba1e14850e879fb68b3c
TrungLH <trungluuhoang456@gmail.com>
2026-04-12 22:35:37 +0700

    Add NotificationDTO

--- src/main/java/com/trung/chat/safechat/websocket/dto/NotificationDTO.java
+++ src/main/java/com/trung/chat/safechat/websocket/dto/NotificationDTO.java
@@ -1,4 +1,40 @@
 package com.trung.chat.safechat.websocket.dto;
 
 public class NotificationDTO {
+    private String userId;
+    private String conversationId;
+    private String content;
+
+    public NotificationDTO() {
+    }
+
+    public NotificationDTO(String userId, String conversationId, String content) {
+        this.userId = userId;
+        this.conversationId = conversationId;
+        this.content = content;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getConversationId() {
+        return conversationId;
+    }
+
+    public void setConversationId(String conversationId) {
+        this.conversationId = conversationId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
