a8e2095a4288de50d96ad803321ea843822cb0a4
TrungLH <trungluuhoang456@gmail.com>
2026-04-12 22:38:02 +0700

    Add PresenceService for setting and checking status online and offline of users

--- src/main/java/com/trung/chat/safechat/websocket/service/PresenceService.java
+++ src/main/java/com/trung/chat/safechat/websocket/service/PresenceService.java
@@ -1,4 +1,18 @@
 package com.trung.chat.safechat.websocket.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface PresenceService {
+    void setOnline(String userId);
+
+    void setOffline(String userId);
+
+    boolean isOnline(String userId);
+
+    void setActiveConversation(String userId, String conversationId);
+
+    boolean isViewingConversation(String userId, String conversationId);
+
+    void removeUser(String userId);
 }
