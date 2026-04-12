c7b459e300030468fa6a87a014a614e1aa2ce7f8
TrungLH <trungluuhoang456@gmail.com>
2026-04-12 22:35:59 +0700

    Add StatusDTO

--- src/main/java/com/trung/chat/safechat/websocket/dto/StatusDTO.java
+++ src/main/java/com/trung/chat/safechat/websocket/dto/StatusDTO.java
@@ -1,4 +1,30 @@
 package com.trung.chat.safechat.websocket.dto;
 
 public class StatusDTO {
+    private String userId;
+    private String status;
+
+    public StatusDTO() {
+    }
+
+    public StatusDTO(String userId, String status) {
+        this.userId = userId;
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 }
