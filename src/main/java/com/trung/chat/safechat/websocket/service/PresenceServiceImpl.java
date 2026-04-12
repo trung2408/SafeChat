68916e6fcaa2218052528c87f8258fb053b0c07b
TrungLH <trungluuhoang456@gmail.com>
2026-04-12 22:39:45 +0700

    Implement functions of PresenceService

--- src/main/java/com/trung/chat/safechat/websocket/service/PresenceServiceImpl.java
+++ src/main/java/com/trung/chat/safechat/websocket/service/PresenceServiceImpl.java
@@ -1,4 +1,45 @@
 package com.trung.chat.safechat.websocket.service;
 
-public class PresenceServiceImpl {
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+public class PresenceServiceImpl implements PresenceService{
+    private final Set<String> onlineUsers = ConcurrentHashMap.newKeySet();
+    private final Map<String, String> activeConversation = new ConcurrentHashMap<>();
+
+    @Override
+    public void setOnline(String userId) {
+        onlineUsers.add(userId);
+    }
+
+    @Override
+    public void setOffline(String userId) {
+        onlineUsers.remove(userId);
+        activeConversation.remove(userId);
+    }
+
+    @Override
+    public boolean isOnline(String userId) {
+        return onlineUsers.contains(userId);
+    }
+
+    @Override
+    public void setActiveConversation(String userId, String conversationId) {
+        activeConversation.put(userId, conversationId);
+    }
+
+    @Override
+    public boolean isViewingConversation(String userId, String conversationId) {
+        return conversationId.equals(activeConversation.get(userId));
+    }
+
+    @Override
+    public void removeUser(String userId) {
+        onlineUsers.remove(userId);
+        activeConversation.remove(userId);
+    }
 }
